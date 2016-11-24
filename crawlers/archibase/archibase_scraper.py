66f77ee53196919ce1e92dd1b2cabc238f5874c7
Daniel Yamins <dyamins@gmail.com>
2016-11-24 02:07:30 -0500

    archibase scraper

---
+++ crawlers/archibase/archibase_scraper.py
@@ -0,0 +1,50 @@
+import os
+import time
+import numpy as np
+import bs4
+import re
+import json
+
+#to obtain crawlera API key, follow instructions here: https://doc.scrapinghub.com/crawlera.html
+KEY = os.environ['CRAWLERA_API_KEY']
+
+def get_models(i):
+    """will get 3d models on page i 
+          model data in zip file
+          model metadata stored in .json file
+    """
+    linkurl = "http://archibaseplanet.com/gdl?category=0&page=%d" % (24 * i + 1)
+    topath = 'links_%d.html' % i
+    os.system('wget "%s" -O %s' % (linkurl, topath))
+    
+    soup = bs4.BeautifulSoup(open(topath))
+
+    model_base = 'http://www.archibaseplanet.com/download/count'
+    data_base = 'http://www.archibaseplanet.com/download'
+
+    model_hrefs = [model_base + '/' + x.findAll('a')[1].attrs['href'].split('/')[-1] for x in soup.findAll('table', 'preview-pad')[0].findAll('td')]
+    data_hrefs = [data_base + '/' + x.findAll('a')[1].attrs['href'].split('/')[-1] for x in soup.findAll('table', 'preview-pad')[0].findAll('td')]
+    
+    reg = re.compile('location: ([\S]+)')
+
+    for h, d in zip(model_hrefs, data_hrefs):
+        hs = h.split('/')[-1].split('.')[0]
+        os.system("curl -vs -x  proxy.crawlera.com:8010 -U %s: %s &> /dev/stdout | tee -a %s.out" % (KEY, h, hs))
+        s = open(hs + '.out', 'rU').read()
+        url = reg.search(s).groups()[0]
+        os.system('wget %s -O %s.zip' % (url, hs))
+        time.sleep(np.random.randint(5))
+        datapath = '%s_data.html' % hs
+        os.system('wget %s -O %s' % (d, datapath))
+        dsoup = bs4.BeautifulSoup(open(datapath))
+        c1, c2 = dsoup.findAll('title')[0].text.split(' | ')
+        name = c1.strip()
+        cat = c2.strip().split('Category: ')[-1]
+        tags = [a.text for a in dsoup.findAll('a') if 'gdl/tag/' in a.attrs['href']]
+        data = {'category': cat, 'name': name, 'tags': tags}
+        with open('%s.json' % hs, 'w') as _f:
+            json.dump(data, _f)
+        
+        
+    
+
