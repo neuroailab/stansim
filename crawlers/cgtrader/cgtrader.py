c7629c77ed4e70378f4ff30efc4ec85fedc0f969
Henryk Błasiński <hblasins@stanford.edu>
2016-12-19 17:36:05 +0100

    Added a few more functions.

--- crawlers/cgtrader/cgtrader.py
+++ crawlers/cgtrader/cgtrader.py
@@ -20,7 +20,7 @@ class CGTraderClient():
             client_secret=client_secret,
             access_token_url="https://api.cgtrader.com/oauth/token",
             authorize_url="https://api.cgtrader.com/oauth/authorize",
-            base_url="https://api.cgtrader.com",
+            base_url=base_url,
         )
 
     def test_api(self):
@@ -38,8 +38,91 @@ class CGTraderClient():
                                      "per_page": 100}).json()
 
         print(len(res['models']))
+
+    def list_categories(self):
+        data = {
+            'grant_type': 'client_credentials',
+                'redirect_uri': '..'
+            }
         
+        session = self.service.get_auth_session(data=data, decoder=json.loads)
         
+        self.access_token = session.access_token
+        
+        res = session.get('/v1/categories').json()
+
+        for i in res['categories']:
+            print i
+
+        return res['categories']
+
+    def get_model(self, modelID):
+        data = {
+            'grant_type': 'client_credentials',
+            'redirect_uri': '..'
+            }
+        
+        session = self.service.get_auth_session(data=data, decoder=json.loads)
+        
+        self.access_token = session.access_token
+        
+
+        res = session.get("/v1/models/%i" % modelID,params = { "id": "%i" % modelID })
+
+        # Handle the case when a model with a given ID does not exist.
+        if res.status_code == 200:
+            res = res.json()
+            print "Model %i (%s), price: $%i, files %i, downloadable %r" % (res["id"],res["slug"],res['prices']['download'], len(res['files']),res['downloadable'])
+        else:
+            res = None
+        
+        for i in res["files"]:
+            print i
+        
+        return res
+
+    def get_model_files_list(self, modelID):
+        data = {
+            'grant_type': 'client_credentials',
+            'redirect_uri': '..'
+        }
+        
+        session = self.service.get_auth_session(data=data, decoder=json.loads)
+        
+        self.access_token = session.access_token
+        
+        res = session.get("/v1/models/%i/files" % modelID,params = { "model_id" : "%i" % modelID })
+
+        print res.url
+        print res.status_code
+        # print res.text
+
+
+
+    def get_model_file(self,modelID,fileID):
+        data = {
+            'grant_type': 'client_credentials',
+            'redirect_uri': '..'
+            }
+        
+        session = self.service.get_auth_session(data=data, decoder=json.loads)
+        
+        self.access_token = session.access_token
+
+        res = session.get("/v1/models/%i/files/%i" % (modelID,fileID),
+                          params = {"model_id" : "%i" % modelID, "id": "%i" % fileID})
+
+        print res.status_code
+        print res.is_redirect
+        print res.text
+
+
+
+
 if __name__ == '__main__':
     A = CGTraderClient()
-    A.test_api()
+    # A.test_api()
+    # A.list_categories()
+    mdl = A.get_model(11)
+    A.get_model_files_list(11)
+    A.get_model_file(11,24)
